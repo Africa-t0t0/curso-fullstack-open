19b2315fe08b61d3c3815cff1cf29d6424de4c6a
Africa-t0t0 <jorge.ramirezz@mail.udp.cl>
2025-09-21 19:25:13 -0300

    feature: conditional testing endpoints

--- part5/blog-backend/app.js
+++ part5/blog-backend/app.js
@@ -21,6 +21,11 @@ app.use(middleware.userExtractor)
 
 app.use('/api/blogs', blogRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
